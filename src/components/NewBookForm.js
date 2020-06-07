32a74df9ce598243d792239e9d4fd528b5eea64d
ValeedAnjum <allinone644@gmail.com>
2020-06-07 16:26:16 +0500

    Reducers, Actions, State

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -2,14 +2,13 @@ import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const NewBookForm = () => {
-  const { addBook } = useContext(BookContext);
+  const { dispatch } = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    //console.log(title, author);
-    addBook(title, author);
+    dispatch({ type: 'ADD_BOOK', book: { title, author }});
     setTitle('');
     setAuthor('');
   }
@@ -17,9 +16,9 @@ const NewBookForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="book title" value={title}
-        onChange={(e) => setTitle(e.target.value)} />
+        onChange={(e) => setTitle(e.target.value)} required />
       <input type="text" placeholder="author name" value={author}
-        onChange={(e) => setAuthor(e.target.value)} />
+        onChange={(e) => setAuthor(e.target.value)} required />
       <input type="submit" value="add book" />
     </form>
   );
